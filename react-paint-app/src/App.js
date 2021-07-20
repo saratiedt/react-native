76f57cf4121bf1e76872b76fb9b0f305a49f37cd
saratiedt <saracristinetiedt@gmail.com>
2021-07-19 22:12:20 -0300

    adicionado botões de seleção de cor

--- react-paint-app/src/App.js
+++ react-paint-app/src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
 import React, { useEffect, useRef, useState } from "react";
+import { View } from "react-native";
 
-function App() {
+const App = () => {
   const canvasRef = useRef(null);
   const contextRef = useRef(null);
   const [isDrawing, setIsDrawing] = useState(false);
@@ -42,14 +43,48 @@ function App() {
     contextRef.current.lineTo(offsetX, offsetY);
     contextRef.current.stroke();
   };
-  return (
-    <canvas
-      onMouseDown={startDrawing}
-      onMouseUp={endDrawing}
-      onMouseMove={draw}
-      ref={canvasRef}
-    ></canvas>
-  );
-}
 
-export default App;
+  const selecionaVermelho = () => {
+    const canvas = canvasRef.current;
+    var context = canvas.getContext("2d");
+    context.strokeStyle = "red";
+  };
+
+  const selecionaVerde = () => {
+    const canvas = canvasRef.current;
+    var context = canvas.getContext("2d");
+    context.strokeStyle = "green";
+  };
+
+  const selecionaAzul = () => {
+    const canvas = canvasRef.current;
+    var context = canvas.getContext("2d");
+    context.strokeStyle = "blue";
+  };
+
+  return (
+    <View>
+      <View>
+        <nav className="navbar">
+          <h1>Paint</h1>
+        </nav>
+        <div className="container">
+          <h3>Meu desenho</h3>
+          <div className="botoes">
+            <button onClick={selecionaVermelho} id="vermelho"></button>
+            <button onClick={selecionaVerde} id="verde"></button>
+            <button onClick={selecionaAzul} id="azul"></button>
+          </div>
+        </div>
+        <canvas
+          onMouseDown={startDrawing}
+          onMouseUp={endDrawing}
+          onMouseMove={draw}
+          ref={canvasRef}
+        ></canvas>
+      </View>
+    </View>
+  );
+};
+
+export default React.forwardRef(App);
